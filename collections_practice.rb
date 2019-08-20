20c3693fccf9804058f5153d1a0c264ae9f92a88
clean-command-1851 <ed7901979t@gmail.com>
2019-08-20 12:22:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,8 +2,15 @@ def soft_array_asc(array)
   array.sort
 end 
 
-def soft_array_asc(array)
-  array.sort_by.reverse!
+def soft_array_des(array)
+ 
+ array.sort{ |a,b| b <=> a}
+ 
 end 
+
+def soft_array_char_count(array)
+  array.sort {word.length < (word.length -1)}
+
+end
 
 
